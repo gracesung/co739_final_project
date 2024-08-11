cf34f2459e626f382e75f56aa221ad13bdbb3469
Grace Sung <gracesungca@gmail.com>
2024-08-10 23:33:15 -0400

    clean up p4_free_attempt

--- Co739FinalProject/p4_free_ATTEMPT.lean
+++ Co739FinalProject/p4_free_ATTEMPT.lean
@@ -1,7 +1,4 @@
--- import Mathlib.Topology.Basic
-
--- #check TopologicalSpace
-
+-- This was an attempt at the p4 free graph structure theorem.
 -- import Mathlib.Combinatorics.SimpleGraph.Finite
 import Mathlib.Combinatorics.SimpleGraph.Subgraph
 import Mathlib.Combinatorics.SimpleGraph.Basic
@@ -10,22 +7,27 @@ import Mathlib.Data.Fintype.Card
 #check SimpleGraph
 #check SimpleGraph.singletonSubgraph
 #check SimpleGraph.Subgraph
-
-
---  prove: P4 free graphs structure theorem
-
-
 #check SimpleGraph.Subgraph.IsInduced
--- #check SimpleGraph.Subgraphx
 
--- def p4 (G: SimpleGraph α) :=
---   -- ∃(v1 v2 v3 v4 : α)
---   G.edgeSet
 
+
+--  prove: P4 free graphs structure theorem:
+-- (Corneil, Lerchs, Burlingham, 1981): Let G be a graph.
+--       Then G is a p4-free graph if and only if one of the following hold:
+--        · |V(G)| = 1
+--        · There exist two p4 free graphs, H and H' such that G is the disjoint union of these two graphs
+--        · There exist two p4 free graphs, H and H' such that G is the graph obtained from the disjoint union
+--              of these two graphs, by adding all possible edges between V(H) and V(H')
+
+-- equivalently: G is a p_4 free graph if and only if for every induced subgraph H of G, either |V(H)| = 1 or
+--     V(H) can be partitioned into two sets A, B such that A is either complete or anticomplete to B
+
+-- An attempt at defining what a p4 graph is:
 def isP4 (G : SimpleGraph α) :=
   ∃ (v1 v2 v3 v4 : α),
   G.Adj v1 v2 ∧  G.Adj v2 v3 ∧  G.Adj v3 v4
   ∧ ¬ (G.Adj v1 v3) ∧ ¬ (G.Adj v1 v4) ∧ ¬ (G.Adj v2 v4)
+  -- another attempt below at what p4 could be defined as:
     -- (v1, v2) ∈ G.edgeSet ∧
     -- (v2, v3) ∈ G.edgeSet ∧
     -- (v3, v4) ∈ G.edgeSet ∧
@@ -35,19 +37,14 @@ def isP4 (G : SimpleGraph α) :=
 
 #check isP4
 
+-- defining p4 helps us define p4-free graphs (attempt):
 def isP4Free (G: SimpleGraph α ) :=
   ¬ (isP4 G)
 
+
 -- theorem p4_free {α : Type*} [DecidableEq α] (G : SimpleGraph α) :
---   (isP4 G ↔
---     ∀ (H : SimpleGraph.Subgraph G),
---       (SimpleGraph.Subgraph.IsInduced H  →
---         (H.verts.toFinset_card = 1 ∨
---          ∃ (A B : Finset α),
---            H.verts = A ∪ B ∧
---            (A.card > 0 ∧ B.card > 0) ∧
---            (A.completeOrAnticompleteTo B)) )) := by
+--   (isP4Free G ↔ ∀ (H: SimpleGraph.Subgraph G), (SimpleGraph.Subgraph.IsInduced H) →
+--          H.verts.toFinset_card = 1 ∨ ∃ (A B : Finset α), H.verts = A ∪ B ∧ (completeOrAnticomplete A B)) )) := by
 --            sorry
--- begin
---   -- The proof will go here.
--- end
+
+-- we would have to define the relation of CompleteOrAnticComplete
